b734534555b9fa35a9733cbb8ccd769e06de825c
kevin <kjhilton01@gmail.com>
2021-08-11 21:38:27 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='bow_rentalr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    instal_requires=[
+        'flask',
+    ],
+)
